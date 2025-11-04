2c5e2fa83c421e2db9568d46932e915b0329a387
K Kevin Gladson <joeljoyson2427@gmail.com>
2025-11-04 18:58:51 +0530

    feat: integrate Tailwind CSS and enhance home component layout
    - Added Tailwind CSS and PostCSS dependencies to package.json.
    - Created a new CSS file for the home component with styles for the app container, header, playlists, and responsiveness.
    - Updated home.component.html to include a structured layout with a header, quick playlists section, and a "Made for You" section.
    - Enhanced home.component.ts to initialize greeting and playlist data.
    - Imported Tailwind CSS in styles.css for global styling.
    - Added postcssrc.json configuration for Tailwind CSS.

--- Angular_Spotify/src/app/components/home/home.component.ts
+++ Angular_Spotify/src/app/components/home/home.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterOrSidebarComponent } from "../footer-or-sidebar/footer-or-sidebar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [FooterOrSidebarComponent],
+  imports: [FooterOrSidebarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {
+  
+  }
+    
 
+  greeting = '';
+
+
+ // Quick playlist grid data
+  quickPlaylists = [
+    { title: "Today's Top Hits", image: 'assets/tophits.jpg' },
+    { title: "Dope Labs", image: 'assets/dope.jpg' },
+    { title: "Chill Hits", image: 'assets/chill.jpg' },
+    { title: "Latina to Latina", image: 'assets/latina.jpg' },
+    { title: "Alan Gogoll", image: 'assets/alangogoll.jpg' },
+    { title: "Small Doses with Amanda Seales", image: 'assets/smalldoses.jpg' },
+  ];
+
+
+
+   madeForYou = [
+    {
+      title: 'On Repeat',
+      subtitle: 'Songs you can’t get enough of.',
+      image: 'assets/onrepeat.jpg'
+    },
+    {
+      title: 'Your Discover Weekly',
+      subtitle: 'Your weekly mixtape of fresh music.',
+      image: 'assets/discover.jpg'
+    },
+    {
+      title: 'Mix 1',
+      subtitle: 'Personalized playlist for you.',
+      image: 'assets/mix1.jpg'
+    }
+  ];
 }
