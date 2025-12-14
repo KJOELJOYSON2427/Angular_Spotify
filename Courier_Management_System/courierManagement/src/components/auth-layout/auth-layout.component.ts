2aa639efaf36f56077f2d711e2ef80f8e5ce99bc
K Kevin Gladson <joeljoyson2427@gmail.com>
2025-12-14 14:19:33 +0530

    feat: update layout components with new structure and improved UI elements

--- Courier_Management_System/courierManagement/src/components/auth-layout/auth-layout.component.ts
+++ Courier_Management_System/courierManagement/src/components/auth-layout/auth-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { LoginComponent } from "../login/login.component";
+import { NavbarComponent } from "../layout/navbar/navbar.component";
+import { FooterComponent } from "../layout/footer/footer.component";
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, NavbarComponent, FooterComponent],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.css'
 })
